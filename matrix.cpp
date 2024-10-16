24d4f1e13ba34c9055b533c150c306fc02fc75ce
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-10-16 13:44:56 +0300

    updatemtxcpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,3 +1,28 @@
+#include<iosream>
+#include "matrix.hpp"
 int** createmtx(size_t m,size_t n);
 void delmtx(int** t, size_t m, size_t n);
+void inputmtx(int** t,size_t m, size_t n)
+{
+	for(size_t i{0};i<m;++i)
+{
+	for(size_t j{0};j<n;++j)
+{
+	std::cin >> t[i][j];
+}
+}
 
+};
+void outputmtx(const int*const* t,size_t m, size_t n)
+{
+	for(size_t i{0},i<m,++i)
+{
+	std::cout << t[i][0];
+	for(size_t j{1},j<n,++j)
+{
+	std::cout << " " << t[i][j];
+}
+	std::cout << "\n";
+}
+
+}
