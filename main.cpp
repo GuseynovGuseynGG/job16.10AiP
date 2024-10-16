b9d1b98f486829809b8d845998b2f6424bb3ae80
GuseynovGuseynGG <ganx.gamer@gmail.com>
2024-10-16 13:06:18 +0300

    create/delete matrix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 
+int** createmtx(size_t m,size_t n);
+void delmtx(int** t, size_t m, size_t n);
 int main()
 {
 	size_t M=0,N=0;
 	std::cin >> M >> N;
 	std::cout << M << " " << N <<"\n";
+	int **arr=createmtx(M,N);
+	delmtx(arr,M,N);
 }
 
